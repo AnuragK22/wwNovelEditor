5e7c78ec7304191a6974a0de7e2245dd4d77d930
AnuragK22 <110153871+AnuragK22@users.noreply.github.com>
2024-03-03 19:40:54 +0530

    Create wwElement.vue

---
+++ src/wwElement.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    Drag me to the Novel Editor
+    <Editor class="editor"  v-on:update="onUpdate()"
+      v-on:debouncedUpdate="debouncedUpdate()" />
+  </div>
+</template>
+
+<script>
+import { Editor } from "novel-vue";
+import "novel-vue/dist/style.css";
+export default {
+  components: {
+    Editor
+  },
+  props: {
+    defaultValue: Object
+  },
+  methods: {
+    onUpdate() {
+      console.log("On update Triggered")
+    },
+    debouncedUpdate() {
+      console.log("On Debounced Update")
+    },
+  },
+  computed: {
+    textStyle() {
+      return {
+        color: this.content.textColor,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  color: green
+}
+
+.editor {
+  border: 1px solid grey;
+}</style>
